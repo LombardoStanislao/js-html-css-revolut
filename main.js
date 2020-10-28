ad52e19d5e48e59150cda27f9796df3afb2233d5
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-10-28 19:38:27 +0100

    Finito l'header ed i dropdown

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
 
-    
-}
+    $('.menu-title').mouseenter(function() {
+        $('.dropdown-menu.active').removeClass('active');
+        $(this).next().addClass('active');
+    });
+
+    $('.dropdown-menu').mouseleave(function() {
+        $(this).removeClass('active')
+    });
+});
